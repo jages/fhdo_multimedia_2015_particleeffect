1397dd308264ffc47fa49857f38355be974c6de6
marsmage <geskejan@gmail.com>
2016-01-05 16:40:38 +0100

    Updated Particle class and ParticleState class,

--- ParticleTest/ParticleState.cs
+++ ParticleTest/ParticleState.cs
@@ -6,24 +6,46 @@ using System.Threading.Tasks;
 
 namespace ParticleTest
 {
+    /// <summary>
+    /// Statemanager for particles. also handles removal and addition of particles, arbitrary limit to max particles can be set.
+    /// </summary>
     public class ParticleState
     {
         public Particle[] particles;
         public int particlecount;
+        public int particlelimit;
 
+        /// <summary>
+        /// creates instance of particle state manager
+        /// </summary>
         public ParticleState()
         {
             particlecount = 0;
+            particlelimit = 5119;
             particles = new Particle[5120];
-            particles[0] = new Particle(0f,0f, 1, 0f, 0f, System.Drawing.Color.Red);
+            //particles[0] = new Particle(0f,0f, 1, 0f, 0f, System.Drawing.Color.Red);
+            //depricated, was needed to not have a NullReferenceException in creation of draw thread. Should be solved?
         }
 
+        /// <summary>
+        /// Adds specified particle to particle management - every particle over limit will just not be added.
+        /// </summary>
+        /// <param name="p">Particle that should be added</param>
         public void addparticle(Particle p)
         {
-            particles[particlecount] = p;
-            particlecount++;
+            if (particlecount < particlelimit)
+            {
+                particles[particlecount] = p;
+                particlecount++;
+            }
+        
         }
 
+        /// <summary>
+        /// removes particle with specified index by swapping dead particle with last living particle.
+        /// Last particle in array needs special treatment
+        /// </summary>
+        /// <param name="i">index of particle to be removed</param>
         public void removeparticle(int i)
         {
             if (particles[i] != null && i < particlecount - 1)
